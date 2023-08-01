b207778238d00a84d324d70a46ee5ee092049fa7
Derek <derek141772@gmail.com>
2023-07-31 21:24:56 -0700

    created and populated tables

--- library.py
+++ library.py
@@ -2,7 +2,31 @@ import sqlite3
 import pandas as pd
 import os
 
-filePath = os.path.join(os.getcwd(), "library.db")
+#filePath = os.path.join(os.getcwd(), "library.db")
+conn = sqlite3.connect('library.db')
+cur = conn.cursor()
+
+def checkSchemaExists(cur):
+    # Define the list of tables that should exist in the schema
+    required_tables = [
+        "Items",
+        "Borrows",
+        "Person",
+        "Employee",
+        "Participates",
+        "Event",
+        "Orders",
+        "FutureItems"
+    ]
+
+    # Check if each required table exists in the database
+    for table in required_tables:
+        cur.execute(f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='{table}'")
+        table_exists = cur.fetchone()[0]
+        if not table_exists:
+            return False
+
+    return True
 
 """
 Executes CREATE TABLE statement
@@ -10,11 +34,11 @@ connection: sqlite3 database connection
 tableStatement: SQL statement in string
 """
 def createSchema():
-    if os.path.exists(filePath):
+    conn = sqlite3.connect('library.db')
+    cur = conn.cursor()
+    if checkSchemaExists(cur):
         print("Schema already created.")
     else:
-        conn = sqlite3.connect('library.db')
-        cur = conn.cursor()
         cur.execute('''
                 CREATE TABLE IF NOT EXISTS Items (
                     itemID CHAR(11) PRIMARY KEY,
@@ -24,13 +48,85 @@ def createSchema():
                     available INTEGER
                 )    
             ''')
+        
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS Borrows (
+            borrowID INTEGER PRIMARY KEY,
+            personID CHAR(11),
+            itemID CHAR(11),
+            borrowDate DATE,
+            dateDue DATE,
+            returnDate DATE,
+            fineAmount REAL,
+            FOREIGN KEY (personID) REFERENCES Person(personID),
+            FOREIGN KEY (itemID) REFERENCES Items(itemID)
+            )
+        ''')
+        
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS Person (
+            personID CHAR(11) PRIMARY KEY,
+            firstName CHAR(20),
+            lastName CHAR(20),
+            birthDate DATE
+        )
+    ''')
 
-        conn.commit()
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS Employee (
+            employeeID CHAR(11) PRIMARY KEY,
+            firstName CHAR(20),
+            lastName CHAR(20),
+            personID CHAR(11),
+            FOREIGN KEY (personID) REFERENCES Person(personID)
+        )
+    ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS Participates (
+            eventID CHAR(11),
+            personID CHAR(11),
+            FOREIGN KEY (eventID) REFERENCES Event(eventID),
+            FOREIGN KEY (personID) REFERENCES Person(personID),
+            PRIMARY KEY (eventID, personID)
+        )
+    ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS Event (
+            eventID CHAR(11) PRIMARY KEY,
+            type CHAR(50),
+            audience CHAR(50),
+            date DATE,
+            location CHAR(50)
+        )
+    ''')
+
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Orders (
+                orderID CHAR(11) PRIMARY KEY,
+                employeeID CHAR(11),
+                fID CHAR(11),
+                FOREIGN KEY (employeeID) REFERENCES Employee(employeeID),
+                FOREIGN KEY (fID) REFERENCES FutureItems(fID)
+            )
+        ''')
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS FutureItems (
+            fID CHAR(11) PRIMARY KEY,
+            title CHAR(50),
+            author CHAR(50),
+            type CHAR(50)
+            )
+        ''')
+        print("created tables")
+    conn.commit()
 
     return
     
 def insertInto(conn):
-    cur = conn.cursor()
+    #cur = conn.cursor()
     cur.execute('''
                 INSERT OR IGNORE INTO Items(itemID, title, author, type, available)
                 VALUES  ('1001', 'Love You Forever', 'Robert Munsch', 'Children', 1),
@@ -44,7 +140,67 @@ def insertInto(conn):
                         ('1009', 'Pigs', 'Robert Munsch', 'Children', 1),
                         ('1010', 'The Battle of the Labyrinth', 'Rick Riordan', 'Fantasy', 1)
                 ''')
-    print("Inserted items data")
+    #print("Inserted items into Items")
+
+    cur.execute('''
+        INSERT OR IGNORE INTO Person(personID, firstName, lastName, birthDate)
+        VALUES  ('1001', 'John', 'Doe', '1985-05-10'),
+                ('1002', 'Jane', 'Smith', '1990-12-15'),
+                ('1003', 'Michael', 'Johnson', '1988-07-22'),
+                ('1004', 'Emily', 'Williams', '1995-03-08'),
+                ('1005', 'Daniel', 'Brown', '1982-09-30'),
+                ('1006', 'Olivia', 'Jones', '1998-11-25'),
+                ('1007', 'William', 'Miller', '1989-06-18'),
+                ('1008', 'Sophia', 'Davis', '1993-04-12'),
+                ('1009', 'David', 'Garcia', '1986-02-28'),
+                ('1010', 'Isabella', 'Martinez', '1991-08-05'),
+                ('1011', 'Ella', 'Kim', '1997-09-20'),
+                ('1012', 'Aiden', 'Nguyen', '1984-11-15'),
+                ('1013', 'Chloe', 'Li', '1992-07-12'),
+                ('1014', 'Ethan', 'Tanaka', '1987-03-25'),
+                ('1015', 'Emma', 'Chen', '1994-06-02')
+    ''')
+    #print("Inserted records into Person")
+
+    cur.execute('''
+        INSERT OR IGNORE INTO Employee(employeeID, firstName, lastName, personID)
+        VALUES  ('2001', 'John', 'Doe', '1001'),
+                ('2002', 'Jane', 'Smith', '1002'),
+                ('2003', 'Michael', 'Johnson', '1003'),
+                ('2004', 'Emily', 'Williams', '1004'),
+                ('2005', 'Daniel', 'Brown', '1005')
+    ''')
+    #print("Inserted records into Employee")
+
+    cur.execute('''
+        INSERT OR IGNORE INTO Event(eventID, type, audience, date, location)
+        VALUES ('2001', 'Book Launch', 'Children', '2023-07-31', 'Library Hall'),
+                ('2002', 'Workshop', 'Teens', '2023-08-05', 'Conference Room'),
+                ('2003', 'Reading Session', 'Adults', '2023-08-12', 'Main Reading Area'),
+                ('2004', 'Author Talk', 'All', '2023-08-17', 'Auditorium'),
+                ('2005', 'Storytelling', 'Children', '2023-08-20', 'Children Section'),
+                ('2006', 'Workshop', 'Adults', '2023-08-25', 'Conference Room'),
+                ('2007', 'Book Launch', 'All', '2023-08-30', 'Library Hall'),
+                ('2008', 'Reading Session', 'Teens', '2023-09-02', 'Main Reading Area'),
+                ('2009', 'Author Talk', 'Adults', '2023-09-07', 'Auditorium'),
+                ('2010', 'Storytelling', 'Children', '2023-09-10', 'Children Section')
+                ''')
+    #print("Inserted records into Event")
+    
+    cur.execute('''
+        INSERT OR IGNORE INTO FutureItems(fID, title, author, type)
+        VALUES  ('3001', 'Lord of the Flies', 'William Golding', 'Fiction'),
+                ('3002', 'The Plague', 'Albert Camus', 'Fiction'),
+                ('3003', 'Tao Te Ching', 'Lao Tzu', 'Philosophy'),
+                ('3004', 'The Art of War', 'Sun Tzu', 'Military'),
+                ('3005', 'The Pragmatic Programmer', 'Andrew Hunt, David Thomas', 'Computer Science'),
+                ('3006', 'Clean Code', 'Robert C. Martin', 'Computer Science'),
+                ('3007', 'Design Patterns', 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides', 'Computer Science'),
+                ('3008', 'Introduction to Algorithms', 'Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein', 'Computer Science'),
+                ('3009', 'Artificial Intelligence: A Modern Approach', 'Stuart Russell, Peter Norvig', 'Computer Science'),
+                ('3010', 'Data Science from Scratch', 'Joel Grus', 'Computer Science')
+                ''')
+    #print("Inserted records into FutureItems")
     conn.commit()
 
 def insertData(connection, insertStatement):
@@ -112,9 +268,9 @@ def searchItems(connection, title, author, id = '-1'):
 
 if __name__ == "__main__":
     createSchema()
-    insertInto()
+    insertInto(conn)
 
-    cur.execute("SELECT * FROM Items")
+    cur.execute("SELECT * FROM Person")
     rows = cur.fetchall()
 
     print(rows)
